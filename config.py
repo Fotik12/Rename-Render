a8d4d706b35ba1756a9f9426e08439522e0027f7
Fotik12 <totonmondal65@gmail.com>
2024-06-20 17:20:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,29 +7,27 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "27919240")
+API_ID = os.environ.get("API_ID", "40")
 
-API_HASH = os.environ.get("API_HASH", "1570036a734bd37515b90b709260045a")
+API_HASH = os.environ.get("API_HASH", ") 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6900304306:AAGCRVRgjNLeMQaeTIO1oBVVPZwCIh8htvw") 
+FORCE_SUB = os.environ.get("FORCE_SUB", 07") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "moviebox0007") 
-
-             # Don't Remove Credit @moviebox0007
+             # Don't Remove Credit 07
              # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
              # Ask Doubt on telegram @KingVJ01
 
 DB_NAME = os.environ.get("DB_NAME", "renamevjbot")     
 
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://Tach:123@fotik@321@cluster0.3swxciv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URL = true&w=majority&appName=Cluster0")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
-START_PIC = os.environ.get("START_PIC", "https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg")
+b6a1.jpg")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '2211770620').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
 
-PORT = os.environ.get("PORT", "8080")
+PORT = os.environ.get("PORT", "0")
 
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
